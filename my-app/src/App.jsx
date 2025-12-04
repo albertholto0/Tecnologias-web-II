c499ca2d719ea8e01792ec8f35a41cf4ed3ab761
albertholto0 <albertmendoza019@gmail.com>
2025-12-03 20:30:50 -0600

    Se creó un proyecto con React

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Gallery from './gallery.jsx';
+import { Profile } from './Profile.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Profile />
+      <Gallery />
+    </>
+  )
+}
---
+++ my-app/src/gallery.jsx
@@ -0,0 +1,12 @@
+import { Profile } from './Profile.jsx';
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Científicos increíbles</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
